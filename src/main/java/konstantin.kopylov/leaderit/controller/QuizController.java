c9764a401b573725a177b6b96f2cd7a6cf0ee698
Melatori <Melatori@gmail.com>
2019-12-27 17:37:23 +0700

    Add sorting (pageable) and filters

--- src/main/java/konstantin.kopylov/leaderit/controller/QuizController.java
+++ src/main/java/konstantin.kopylov/leaderit/controller/QuizController.java
@@ -3,16 +3,23 @@ package konstantin.kopylov.leaderit.controller;
 import konstantin.kopylov.leaderit.entities.Quiz;
 import konstantin.kopylov.leaderit.repository.QuizRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/quiz")
 public class QuizController {
+
+    private static final int PAGE_SIZE = 100;
 
     @Autowired
     private QuizRepository quizRepository;
@@ -20,6 +27,46 @@ public class QuizController {
     @GetMapping
     public ResponseEntity<Iterable<Quiz>> getAllQuizzes() {
         return ResponseEntity.ok().body(quizRepository.findAll());
+    }
+
+    @GetMapping("/sorted")
+    public ResponseEntity<Iterable<Quiz>> getFilteredQuizzes(@RequestParam String sortBy,
+                                                             @RequestParam String sortOrder,
+                                                             @RequestParam(required = false) String name,
+                                                             @RequestParam(required = false) Date startDate,
+                                                             @RequestParam(required = false) Date endDate,
+                                                             @RequestParam(required = false) Boolean active) {
+        Iterable<Quiz> foundQuizzes;
+        Quiz quiz = new Quiz();
+        quiz.setName(name);
+        quiz.setStartDate(startDate);
+        quiz.setEndDate(endDate);
+        quiz.setActive(active);
+
+        if (!(sortBy.equals("name") || sortBy.equals("startDate"))) {
+            foundQuizzes = quizRepository.findAll(Example.of(quiz));
+            return ResponseEntity.ok().body(foundQuizzes);
+        }
+
+        Pageable page;
+
+        if (sortBy.equals("name")) {
+            if (sortOrder.equals("DESC")) {
+                page = PageRequest.of(0, PAGE_SIZE, Sort.by(Direction.DESC, "name"));
+            } else {
+                page = PageRequest.of(0, PAGE_SIZE, Sort.by(Direction.ASC, "name"));
+            }
+        } else {
+            if (sortOrder.equals("DESC")) {
+                page = PageRequest.of(0, PAGE_SIZE, Sort.by(Direction.DESC, "startDate"));
+            } else {
+                page = PageRequest.of(0, PAGE_SIZE, Sort.by(Direction.ASC, "startDate"));
+            }
+        }
+
+        foundQuizzes = quizRepository.findAll(Example.of(quiz), page).getContent();
+
+        return ResponseEntity.ok().body(foundQuizzes);
     }
 
     @GetMapping("/{id}")
@@ -38,7 +85,6 @@ public class QuizController {
         Optional<Quiz> searchResult = quizRepository.findById(id);
 
         return searchResult.map(foundQuiz -> {
-            System.out.println("found");
             foundQuiz.setName(quiz.getName());
             foundQuiz.setStartDate(quiz.getStartDate());
             foundQuiz.setEndDate(quiz.getEndDate());
--- src/main/java/konstantin.kopylov/leaderit/repository/QuizRepository.java
+++ src/main/java/konstantin.kopylov/leaderit/repository/QuizRepository.java
@@ -1,14 +1,17 @@
 package konstantin.kopylov.leaderit.repository;
 
 import konstantin.kopylov.leaderit.entities.Quiz;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
-public interface QuizRepository extends CrudRepository <Quiz, Long> {
+@Repository
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
     List<Quiz> findByName(String name);
     List<Quiz> findByStartDate(Date date);
     List<Quiz> findByEndDate(Date date);
     List<Quiz> findByActive(Boolean active);
+    List<Quiz> findByNameAndStartDate(String name, Date date);
 }
